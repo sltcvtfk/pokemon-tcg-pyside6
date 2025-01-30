b7cd1264ae2dca638b7f18312f1971bec3614068
natslol <nqtsishere@gmail.com>
2025-01-30 17:23:43 +0100

    affichage (booster + carte)

---
+++ booster.py
@@ -0,0 +1,56 @@
+from PySide6.QtGui import QPixmap, QImage
+from PySide6.QtCore import Qt
+import requests
+import json
+import random
+
+carte_dict = {
+    "Fighting": "img/Fighting.png",
+    "Dragon": "img/Dragon.png",
+    "Water": "img/Water.png",
+    "Electric": "img/Electric.png",
+    "Fire": "img/Fire.png",
+    "Dark": "img/Dark.png",
+    "Water": "img/Water.png",
+    "Grass": "img/Grass.png",
+    "Normal": "img/Normal.png",
+    "Steel": "img/Steel.png",
+    "Poison": "img/Grass.png",
+    "Ice": "img/Water.png",
+    "Rock": "img/Fighting.png",
+    "Ground": "img/Fighting.png",
+    "Fairy": "img/Psychic.png",
+    "Flying": "img/Normal.png",
+    "Bug": "img/Grass.png",
+    "Ghost": "img/Psychic.png",
+}
+
+with open('pokedex.json', encoding="utf8") as f:
+    res = json.load(f)
+    
+def charge_carte_image(pokedex_id: int):
+    with open(carte_dict[res[pokedex_id-1]["type"][0]], 'rb') as image:
+        img = QImage()
+        img.loadFromData(image.read())
+        img = img.scaled(300, 560, Qt.AspectRatioMode.KeepAspectRatio)
+        pixmap = QPixmap.fromImage(img)
+        return pixmap
+
+def charge_pokemon_image(pokedex_id: int):
+    x = requests.get(res[pokedex_id-1]["image"]["hires"], stream=True)
+    image = QImage()
+    image.loadFromData(x.content)
+    img = image.scaled(130, 130, Qt.AspectRatioMode.KeepAspectRatio)
+    pixmap = QPixmap.fromImage(img)
+    return pixmap
+
+def affiche_booster():
+    with open("img/booster.png", 'rb') as image:
+        img = QImage()
+        img.loadFromData(image.read())
+        img = img.scaled(300, 510, Qt.AspectRatioMode.KeepAspectRatio)
+        pixmap = QPixmap.fromImage(img)
+        return pixmap
+    
+def start_booster():
+    print("start booster")
--- main.py
+++ main.py
@@ -5,31 +5,24 @@ from PySide6.QtWidgets import (
     QGraphicsRectItem,
     QGraphicsScene,
     QGraphicsView,
-    QGraphicsItem
+    QGraphicsPixmapItem
 )
-from PySide6.QtNetwork import (QNetworkAccessManager, )
-import requests
-import json
+from PySide6.QtCore import Qt
+from booster import *
 
+#class Pokemon(): 
 
-with open('pokedex.json', encoding="utf8") as f:
-    res = json.load(f)
+        
 
-x = requests.get(res[0]["image"]["hires"], stream=True)
-
-class Label_image(QLabel):
-    def __init__(self):
+class Bouton(QPushButton):
+    def __init__(self, parent=None):
         super().__init__()
-        image = QImage()
-        image.loadFromData(x.text)
-        pixmap = QPixmap.fromImage(image)
-        self.setPixmap(pixmap)
+        
 
 class Scene(QGraphicsScene):
     def __init__(self, *args): 
         super().__init__(*args)
-        self.rect = QGraphicsRectItem(0, 0, 300, 475)
-        
+        self.rect = QGraphicsRectItem(0, 0, 375, 680)
         self.rect.setPos(10, 10)
         brush = QBrush(QColor(220,220,220))
         self.rect.setBrush(brush)
@@ -37,6 +30,7 @@ class Scene(QGraphicsScene):
         pen.setWidth(1)
         self.rect.setPen(pen)
         self.addItem(self.rect)
+        
     #def setColor(self, *args):
      #   brush = QBrush(QColor())
     
@@ -47,12 +41,26 @@ class MyWindow(QMainWindow):
         super().__init__()
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
-        self.scene = Scene(0,0,325,500)
-        view = QGraphicsView(self.scene)
+        self.scene = Scene(0,0,400,700)
         
+        # self.scene.addPixmap(charge_carte_image(pokemon_id))
+        # self.scene.addPixmap(charge_pokemon_image(pokemon_id))
+        booster = self.scene.addPixmap(affiche_booster())
+        self.scene.items()[0].setPos(60,50)
+        # self.scene.removeItem(booster)
+        # self.scene.items()[0].setPos(130,100)
+        view = QGraphicsView(self.scene)
         self.layout = QHBoxLayout()
+        self.open_button = QPushButton("Open!")
+        self.open_button.setFixedSize(50, 50)
+        self.open_button.setGeometry(175, 570, 50, 50)
+        self.open_button.clicked.connect(start_booster)
+        self.scene.addWidget(self.open_button)
+        
+        
+        
         self.layout.addWidget(view)
-        self.layout.addWidget(Label_image)
+      
         
         centralWidget.setLayout(self.layout)
 
