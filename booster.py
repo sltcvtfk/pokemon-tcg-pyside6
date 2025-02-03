5b7fb87d9a72f195c89247b3b22d7e49dfc55ef6
natslol <nqtsishere@gmail.com>
2025-02-03 23:40:11 +0100

    Added previous evolution and card if basic, stage 1 or stage 2

--- booster.py
+++ booster.py
@@ -13,67 +13,171 @@ class Pokemon():
     def __init__(self, pokedex_id):
         self.pokedex_id = pokedex_id
         
-    def description(self):
-        return res[self.pokedex_id-1]["description"]
-        
     def height(self):
+        """
+            Returns:
+                int: Height of the pokemon
+        """
         return res[self.pokedex_id-1]["profile"]["height"]
     
     def weight(self):
+        """
+            Returns:
+                int: Weight of the pokemon
+        """
+        
         return res[self.pokedex_id-1]["profile"]["weight"]
         
     def species(self):
+        """
+            Returns:
+                str: Species of the pokemon
+        """
         return res[self.pokedex_id-1]["species"]
     
     def english_name(self):
+        """
+            Returns:
+                str: English name of the pokemon
+        """
         return res[self.pokedex_id-1]["name"]["english"]
-    
+
     def french_name(self):
+        """
+            Returns:
+                str: French name of the pokemon
+        """
         return res[self.pokedex_id-1]["name"]["french"]
     
     def hp(self):
+        """
+        Returns:
+            int: HP of the pokemon
+        """
         return res[self.pokedex_id-1]["base"]["HP"]
     
     def description(self): 
+        """
+            Returns:
+                str: Description of the pokemon
+        """
         return res[self.pokedex_id-1]["description"]
     
     def his_type(self):
+        """
+            Returns:
+                str: Type of the pokemon
+        """
         return TYPE_DICT[res[self.pokedex_id-1]["type"][0]]
     
     def weakness(self):
+        """
+        Returns:
+            str: Weakness of the pokemon
+        """
         return WEAKNESS_TYPE_DICT[TYPE_DICT[res[self.pokedex_id-1]["type"][0]]]
     
     def generation(self):
+        """
+            Returns:
+                int: Generation of the pokemon
+        """
         for i, (start, end) in enumerate(GENERATION):
             if self.pokedex_id >= start and self.pokedex_id <= end:
-                return i+1
+                return i+1     
+    
+    def has_prev_evolution(self):
+        """ 
+            Returns: 
+                bool: True if the pokemon has a previous evolution
+        """
+        if len(res[self.pokedex_id-1]["evolution"]) != 0:
+            if "prev" in res[self.pokedex_id-1]["evolution"]:
+                return True
+        return False
+    
+    def prev_evolution(self):
+        """
+            Returns:
+                int: Pokedex ID of the previous evolution
+        """
+        return int(res[self.pokedex_id-1]["evolution"]["prev"][0])
+        
+    def prev_evo_has_prev_evo(self):
+        """ 
+            Returns: 
+                bool: True if the previous evolution has a previous evolution
+        """
+        prev = self.prev_evolution()
+        if "prev" in res[prev-1]["evolution"]:
+            return True
+        return False
+        
     def __repr__(self):
         return f"{self.english_name()} ({self.french_name()})\nPV: {self.hp()}\nType: {self.his_type()}\nWeakness: {self.weakness()}\nGeneration: {self.generation()}"
-      
 
 
 class Booster():
     def __init__(self):
-        self.pokemon = []
+        self.pokemons = []
         
     def add_to_list(self, pokedex_id):
         self.pokemon.append(Pokemon(pokedex_id))
         
-    def load_carte_image(self, pokedex_id: int):
-        with open(CARTE_DICT[res[pokedex_id-1]["type"][0]], 'rb') as image:
-            img = QImage()
-            img.loadFromData(image.read())
-            img = img.scaled(300, 560, Qt.AspectRatioMode.KeepAspectRatio)
-            return img
+    def load_carte_image(self, pokedex_id: int, how_many_prev: int):
+        """
+            Returns:
+                QImage: Image of the card
+        """
+        
+        if how_many_prev == 0:
+            with open(BASIC_CARD_DICT[res[pokedex_id-1]["type"][0]], 'rb') as image:
+                img = QImage()
+                img.loadFromData(image.read())
+                img = img.scaled(300, 560, Qt.AspectRatioMode.KeepAspectRatio)
+                return img
+        elif how_many_prev == 1:
+            with open(STAGE1_CARD_DICT[res[pokedex_id-1]["type"][0]], 'rb') as image:
+                img = QImage()
+                img.loadFromData(image.read())
+                img = img.scaled(300, 560, Qt.AspectRatioMode.KeepAspectRatio)
+                return img
+        elif how_many_prev == 2:
+            with open(STAGE2_CARD_DICT[res[pokedex_id-1]["type"][0]], 'rb') as image:
+                img = QImage()
+                img.loadFromData(image.read())
+                img = img.scaled(300, 560, Qt.AspectRatioMode.KeepAspectRatio)
+                return img
 
     def load_pokemon_image(self, pokedex_id: int):
+        """
+            Returns:
+                QImage: Image of the pokemon
+        """
         x = requests.get(res[pokedex_id-1]["image"]["hires"], stream=True)
         image = QImage()
         image.loadFromData(x.content)
         img = image.scaled(130, 130, Qt.AspectRatioMode.KeepAspectRatio)
         return img
     
+    def load_prev_evolution_image(self, pokemon: Pokemon):
+        """
+            Returns:
+                QImage: Image of the previous evolution
+        """
+        self.prev = pokemon.prev_evolution()
+        if self.prev != None:
+            x = requests.get(res[self.prev-1]["image"]["sprite"], stream=True)
+            image = QImage()
+            image.loadFromData(x.content)
+            img = image.scaled(40, 40, Qt.AspectRatioMode.KeepAspectRatio)
+            return img
+    
     def load_pokemon_weakness_retreat_image(self, pokedex_id: int):
+        """
+            Returns:
+                tuple: Images of the weakness and retreat
+        """
         with open(WEAKNESS_IMAGE_DICT[TYPE_DICT[res[pokedex_id-1]["type"][0]]], 'rb') as image:
             weakness = QImage()
             weakness.loadFromData(image.read())
@@ -83,20 +187,37 @@ class Booster():
             retreat.loadFromData(image.read())
             retreat = retreat.scaled(13, 13, Qt.AspectRatioMode.KeepAspectRatio)
         return weakness, retreat
-        
     
 
     def creation_carte_pokemon(self, pokedex_id: int):
-        carte = self.load_carte_image(pokedex_id)
+        """
+            Returns:
+                QPixmap: Image of the card with the pokemon, its previous evolution, its weakness and retreat, its HP, its name, its height and weight and its species
+        """
+        
+        my_pokemon = Pokemon(pokedex_id)
+        
+        if my_pokemon.has_prev_evolution():
+            if my_pokemon.prev_evo_has_prev_evo():
+                carte = self.load_carte_image(pokedex_id, 2)
+            else:
+                carte = self.load_carte_image(pokedex_id, 1)
+            self.prev_evo = self.load_prev_evolution_image(my_pokemon)
+        else:
+            carte = self.load_carte_image(pokedex_id, 0)
+            self.prev_evo = None
+
         new_pokemon = self.load_pokemon_image(pokedex_id)
         weakness_retreat = self.load_pokemon_weakness_retreat_image(pokedex_id)
-        my_pokemon = Pokemon(pokedex_id)
+        
         # Assembler les deux images
         painter = QPainter(carte)
-        painter.drawImage(90, 50, new_pokemon)
+        painter.drawImage(85, 60, new_pokemon)
         painter.setFont(HP_FONT)
-        painter.drawText(50, 210, f"N°{pokedex_id:04d} {my_pokemon.species()} HT: {my_pokemon.height()} WT: {my_pokemon.weight()} ") 
-
+        painter.drawText(50, 210, f"N°{pokedex_id:03d} {my_pokemon.species()} HT: {my_pokemon.height()} WT: {my_pokemon.weight()} ") 
+        
+        if self.prev_evo != None:
+            painter.drawImage(10, 27, self.prev_evo)    
         if my_pokemon.his_type() == "Dark":
             painter.setPen(Qt.white)
         else:
@@ -124,6 +245,10 @@ class Booster():
         return pixmap
     
     def affiche_booster(self):
+        """
+            Returns:
+                QPixmap: Image of the booster
+        """
         with open(BOOSTER, 'rb') as image:
             img = QImage()
             img.loadFromData(image.read())
--- main.py
+++ main.py
@@ -21,6 +21,7 @@ class Bouton(QPushButton):
 class Scene_Booster(QGraphicsScene):
     def __init__(self, *args): 
         super().__init__(*args)
+
         self.rect = QGraphicsRectItem(0, 0, 375, 680)
         self.rect.setPos(10, 10)
         brush = QBrush(QColor(220,220,220))
@@ -29,7 +30,7 @@ class Scene_Booster(QGraphicsScene):
         pen.setWidth(1)
         self.rect.setPen(pen)
         self.addItem(self.rect)
-        
+
     #def start_booster(self):     
     #def setColor(self, *args):
     #brush = QBrush(QColor())
@@ -42,7 +43,11 @@ class Button_Open(QPushButton):
         self.setFixedSize(50, 50)
         self.compte = 0   
         
-    def ajoute(self):
+    def click(self):
+        """
+        Returns:
+            int: the number of times the button has been clicked
+        """
         self.compte += 1
         return self.compte
 
@@ -52,7 +57,7 @@ class MyWindow(QMainWindow):
         centralWidget = QWidget()
         self.setCentralWidget(centralWidget)
         self.scene = Scene_Booster(0,0,400,700)
-    
+
         self.booster = Booster()
         self.boosterPixmap = self.scene.addPixmap(self.booster.affiche_booster())
         self.boosterPixmap.setPos(60,50)
@@ -63,19 +68,16 @@ class MyWindow(QMainWindow):
         self.open_button.setFixedSize(50, 50)
         self.open_button.setGeometry(175, 570, 50, 50)
 
-        self.open_button.clicked.connect(self.sltcv)
+        self.open_button.clicked.connect(self.booster_scene)
         self.scene.addWidget(self.open_button)
-        
-        
-        
         self.layout.addWidget(view)
-      
         
         centralWidget.setLayout(self.layout)
         
     @Slot()
-    def sltcv(self):
-        self.open_button.ajoute()
+    def booster_scene(self):
+
+        self.open_button.click()
         print(self.open_button.compte)
         if(self.open_button.compte == 1):
             self.scene.removeItem(self.boosterPixmap)
@@ -95,8 +97,6 @@ class MyWindow(QMainWindow):
 class Button_Scene(QPushButton):
     def __init__(self):
         pass
-
-       
 
 if __name__ == "__main__":
     app = QApplication([])
